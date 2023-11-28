83ba3006d7d1d406f6878fb198d1afdbcd500a09
Jamieson Reinhard <jamiesonreinhard@Jamiesons-MacBook-Pro.local>
2023-11-28 16:20:59 -0600

    add .env to gitignore

--- src/app/_components/Chatbot.tsx
+++ src/app/_components/Chatbot.tsx
@@ -7,7 +7,7 @@ import InputArea from ".//InputArea";
 
 function Chatbot() {
     return (
-        <div className="w-[400px] min-h-[50vh] max-h-[70vh] flex flex-col justify-between p-8 rounded-[16px] shadow-xl">
+        <div className="w-[400px] min-h-[50vh] max-h-[70vh] flex flex-col justify-between p-8 rounded-[16px] shadow-xl bg-white">
             <ChatHeader></ChatHeader>
             <MessageWindows></MessageWindows>
             <InputArea></InputArea>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,23 @@
 // src/page.tsx
 "use client";
 import Chatbot from "./_components/Chatbot";
+import ProfileImage from "@/assets/jamiesonReinhard.webp";
+import Image from "next/image";
 
 export default function Home() {
   
   return (
     <main
       id="chatbot-openai"
-      className="w-screen h-screen bg-white shadow-lg overflow-hidden flex justify-center py-[120px]"
+      className="w-screen h-screen bg-white shadow-lg overflow-hidden flex bg-gray-100"
     >
-      <Chatbot />
+      <div className="flex flex-col justify-center items-center w-1/2 p-4">
+        <Image src={ProfileImage} alt="Your Name" className="rounded-full w-32 h-32 mb-4" /> {/* Update alt with your name */}
+        <p className="text-center text-sm">Short bio goes here. Another sentence about you.</p> {/* Update with your bio */}
+      </div>
+      <div className="w-1/2 flex justify-center py-[120px]">
+        <Chatbot />
+      </div>
     </main>
   );
 }
-
-const LoadingSpinner = () => {
-  return (
-    <div className="flex justify-center items-center">
-      <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-gray-900"></div>
-    </div>
-  );
-};
